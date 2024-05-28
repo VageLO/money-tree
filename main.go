6c845412bdb62f0156fe3c25212a1c3a0142696c
VageLO <48802151+VageLO@users.noreply.github.com>
2024-05-28 17:45:05 +0300

    Init

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+type Slide func(nextSlide func()) (title string, content tview.Primitive)
+
+var app = tview.NewApplication()
+
+func main() {
+	// The presentation slides.
+	slides := []Slide{
+		TransactionsTable,
+	}
+
+	pages := tview.NewPages()
+
+	// The bottom row has some info on where we are.
+	info := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetWrap(false).
+		SetHighlightedFunc(func(added, removed, remaining []string) {
+			if len(added) == 0 {
+				return
+			}
+
+			pages.SwitchToPage(added[0])
+		})
+
+	// Create the pages for all slides.
+	previousSlide := func() {
+		if len(slides) <= 1 {return}
+		slide, _ := strconv.Atoi(info.GetHighlights()[0])
+		slide = (slide - 1 + len(slides)) % len(slides)
+		info.Highlight(strconv.Itoa(slide)).
+			ScrollToHighlight()
+	}
+	nextSlide := func() {
+		if len(slides) <= 1 {return}
+		slide, _ := strconv.Atoi(info.GetHighlights()[0])
+		slide = (slide + 1) % len(slides)
+		info.Highlight(strconv.Itoa(slide)).
+			ScrollToHighlight()
+	}
+	for index, slide := range slides {
+		title, primitive := slide(nextSlide)
+		pages.AddPage(strconv.Itoa(index), primitive, true, index == 0)
+		fmt.Fprintf(info, `%d ["%d"][darkcyan]%s[white][""]  `, index+1, index, title)
+	}
+	info.Highlight("0")
+
+	// Create the main layout.
+	layout := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(pages, 0, 1, true).
+		AddItem(info, 1, 1, false)
+
+	// Shortcuts to navigate the slides.
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyCtrlN {
+			nextSlide()
+			return nil
+		} else if event.Key() == tcell.KeyCtrlP {
+			previousSlide()
+			return nil
+		}
+		return event
+	})
+
+	// Start the application.
+	if err := app.SetRoot(layout, true).EnableMouse(true).EnablePaste(true).Run(); err != nil {
+		panic(err)
+	}
+}
