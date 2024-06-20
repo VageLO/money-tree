80f7cf0c702369a3f8a5ba594b449ecc98b5a1fe
VageLO <48802151+VageLO@users.noreply.github.com>
2024-06-20 17:21:18 +0300

    update: sql triggers

--- sql/Update_Balance_On_Transaction_Update.sql
+++ sql/Update_Balance_On_Transaction_Update.sql
@@ -25,26 +25,39 @@ BEGIN
 		WHEN new.account_id = old.account_id
 		THEN CASE
 			WHEN new.transaction_type = 'debit' AND old.transaction_type = 'debit'
-			THEN ((SELECT balance FROM Accounts WHERE id = old.account_id) + old.amount) - new.amount
+			THEN IIF((((SELECT balance FROM Accounts WHERE id = old.account_id) + old.amount) - new.amount) < 0,
+				RAISE(ABORT, "ERROR: Not enough money"),
+				((SELECT balance FROM Accounts WHERE id = old.account_id) + old.amount) - new.amount)
 			
 			WHEN new.transaction_type = 'debit' AND old.transaction_type = 'credit'
-			THEN ((SELECT balance FROM Accounts WHERE id = new.account_id) + old.amount) + new.amount
+			THEN IIF((((SELECT balance FROM Accounts WHERE id = old.account_id) - old.amount) - new.amount) < 0,
+				RAISE(ABORT, "ERROR: Not enough money"),
+				((SELECT balance FROM Accounts WHERE id = old.account_id) - old.amount) - new.amount)
 			
 			WHEN new.transaction_type = 'credit' AND old.transaction_type = 'debit'
-			THEN ((SELECT balance FROM Accounts WHERE id = new.account_id) - old.amount) - new.amount
+			THEN IIF((((SELECT balance FROM Accounts WHERE id = new.account_id) + old.amount) + new.amount) < 0, 
+				RAISE(ABORT, "ERROR: Not enough money"), 
+				((SELECT balance FROM Accounts WHERE id = new.account_id) + old.amount) + new.amount)
 			
 			WHEN new.transaction_type = 'credit' AND old.transaction_type = 'credit'
-			THEN ((SELECT balance FROM Accounts WHERE id = new.account_id) - old.amount) + new.amount
+			THEN IIF((((SELECT balance FROM Accounts WHERE id = new.account_id) - old.amount) + new.amount) < 0,
+				RAISE(ABORT, "ERROR: Not enough money"), 
+				((SELECT balance FROM Accounts WHERE id = new.account_id) - old.amount) + new.amount)
 			
-			ELSE RAISE(ABORT, "fdf")
+			ELSE RAISE(ABORT, "ERROR: ELSE UPDATE Accounts new=old")
 			END
 		WHEN new.account_id <> old.account_id
 		THEN CASE 
 			WHEN new.transaction_type = 'debit'
-			THEN (SELECT balance FROM Accounts WHERE id = new.account_id) - new.amount
-			
+			THEN IIF(((SELECT balance FROM Accounts WHERE id = new.account_id) - new.amount) < 0,
+				RAISE(ABORT, "ERROR: Not enough money"), 
+				(SELECT balance FROM Accounts WHERE id = new.account_id) - new.amount)
+
 			WHEN new.transaction_type = 'credit'
-			THEN (SELECT balance FROM Accounts WHERE id = new.account_id) + new.amount
+			THEN IIF(((SELECT balance FROM Accounts WHERE id = new.account_id) + new.amount) < 0,
+				RAISE(ABORT, "ERROR: Not enough money"), 
+				(SELECT balance FROM Accounts WHERE id = new.account_id) + new.amount)
+
 			ELSE RAISE(ABORT, "Error: Update Account new.account_id")
 			END
 
