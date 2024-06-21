422c5433dc02834433486e94b754a9dad62607e6
VageLO <48802151+VageLO@users.noreply.github.com>
2024-06-21 10:38:35 +0300

    sql: before delete balance column

--- sql/Update_Balance_On_Transaction_Insert.sql
+++ sql/Update_Balance_On_Transaction_Insert.sql
@@ -4,11 +4,28 @@ FOR EACH ROW
 
 BEGIN
 	UPDATE Transactions
-	SET balance = (SELECT balance FROM Accounts WHERE id = new.account_id) - new.amount WHERE id = new.id AND new.transaction_type = 'debit';
-	UPDATE Transactions
-	SET balance = (SELECT balance FROM Accounts WHERE id = new.account_id) + new.amount WHERE id = new.id AND new.transaction_type = 'credit';
+	SET balance = CASE
+		WHEN new.transaction_type = 'debit'
+		THEN IIF(((SELECT balance FROM Accounts WHERE id = new.account_id) - new.amount) < 0,
+			RAISE(ABORT, "ERROR: Not enough money"),
+			(SELECT balance FROM Accounts WHERE id = new.account_id) - new.amount)
+		WHEN new.transaction_type = 'credit'
+		THEN IIF(((SELECT balance FROM Accounts WHERE id = new.account_id) + new.amount) < 0,
+			RAISE(ABORT, "ERROR: Not enough money"),
+			(SELECT balance FROM Accounts WHERE id = new.account_id) + new.amount)
+		ELSE RAISE(ABORT, "ERROR: ELSE UPDATE Transactions ON Insert")		
+	END WHERE id = new.id;
+	
 	UPDATE Accounts
-	SET balance = (SELECT balance FROM Accounts WHERE id = new.account_id) - new.amount WHERE id = new.account_id AND new.transaction_type = 'debit';
-	UPDATE Accounts
-	SET balance = (SELECT balance FROM Accounts WHERE id = new.account_id) + new.amount WHERE id = new.account_id AND new.transaction_type = 'credit';
+	SET balance = CASE
+		WHEN new.transaction_type = 'debit'
+		THEN IIF(((SELECT balance FROM Accounts WHERE id = new.account_id) - new.amount) < 0,
+			RAISE(ABORT, "ERROR: Not enough money"),
+			(SELECT balance FROM Accounts WHERE id = new.account_id) - new.amount)
+		WHEN new.transaction_type = 'credit'
+		THEN IIF(((SELECT balance FROM Accounts WHERE id = new.account_id) + new.amount) < 0,
+			RAISE(ABORT, "ERROR: Not enough money"),
+			(SELECT balance FROM Accounts WHERE id = new.account_id) + new.amount)
+		ELSE RAISE(ABORT, "ERROR: ELSE UPDATE Accounts ON Insert")
+	END WHERE id = new.account_id;
 END;
